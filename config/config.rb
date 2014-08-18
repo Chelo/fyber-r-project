edb1adce6a70883a7c9fbeb2e8d1187c52d0733b
Chelo <unoryche@gmail.com>
2014-08-18 12:14:46 -0430

    Trying with all packages

--- config/config.rb
+++ config/config.rb
@@ -4,7 +4,7 @@ $log = Logger.new(STDOUT)
 configure do
   set :packages_url, "http://cran.r-project.org/src/contrib/PACKAGES"
   set :base_url, "http://cran.r-project.org/src/contrib/"
- #set :number_of_packages, 215
+  set :number_of_packages, nil
 end
 
 configure :production do
